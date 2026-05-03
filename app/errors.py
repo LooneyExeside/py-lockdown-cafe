1f8b0b1305296570dd40661997fbccba7eec6926
Looney .exe <dmytro1601@gmail.com>
2026-05-03 17:58:09 +0200

    fix solution

--- app/errors.py
+++ app/errors.py
@@ -3,15 +3,12 @@ class VaccineError(Exception):
 
 
 class OutdatedVaccineError(VaccineError):
-    def __str__(self) -> str:
-        return "OutdatedVaccineError"
+    pass
 
 
 class NotVaccinatedError(VaccineError):
-    def __str__(self) -> str:
-        return "NotVaccinatedError"
+    pass
 
 
 class NotWearingMaskError(Exception):
-    def __str__(self) -> str:
-        return "NotWearingMaskError"
+    pass
